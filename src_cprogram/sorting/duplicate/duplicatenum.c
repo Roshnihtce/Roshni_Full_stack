bd5677ca2b9e10ceb11f3e1160b69c2e4becf675
Roshnihtce <roshnihtce87@gmail.com>
2024-06-19 15:58:54 +0530

    delete

---
+++ src_cprogram/sorting/duplicate/duplicatenum.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int array[10]={1,1,3,1,3,1,1,1,2,1};
+    for(int i=0; i<10; i++)
+    {
+        printf("%d",array[i]);
+    }
+    for(int j=0; j<10; j++)
+    {
+        for(int k=j+1; j<10; j++)
+    }
+
+
+    
+}
